109719b2f248ffcda6a1b1ba857f9419da719d9e
Andrew Lee <andrew.lee@avant.com>
2016-05-24 14:30:45 -0500

    Macro stuff works.

---
+++ lib/sonar/core/api.ex
@@ -0,0 +1,52 @@
+defmodule Sonar.Core.API do
+    @moduledoc ~S"""
+    This module allows you to automatically generate an API for any AWS service through the `using` macro. In your host
+    module, simply define the following module attributes:
+
+        @api_version -- API version in YYYYMMDD format
+        @api_service -- API service name tuple {short_name, long_name} (e.g. {"ecs", "AmazonEC2ContainerService"} for Amazon ECS)
+        @api_methods -- array of API method names to generate
+
+    Then invoke the using macro via `use Sonar.Core.API` and your module will populate with the proper function stubs.
+
+    """
+
+    defmacro __using__(_) do
+        quote unquote: false do
+            alias Sonar.Core
+            alias Sonar.Utils
+            alias Sonar.Utils.Amazon
+
+            for method <- @api_methods do
+                name = method |> Mix.Utils.underscore |> String.to_atom
+
+                def unquote(name)(auth, params \\ []) do
+                    body = params
+                        |> Utils.API.generate_parameter_map
+                        |> Poison.encode!
+
+                    {short_api, long_api} = @api_service
+
+                    %HTTPotion.Response{body: rbody} = Core.Auth.make_request(
+                        auth[:aws_access_key],
+                        auth[:aws_secret_key],
+                        auth[:aws_region] || @default_aws_region,
+                        short_api,
+                        "POST", "https://#{short_api}.#{@default_aws_region}.amazonaws.com/",
+                        body,
+                        [
+                            {"Host", "#{short_api}.#{@default_aws_region}.amazonaws.com"},
+                            {"Accept-Encoding", "identity"},
+                            {"Content-Length", String.length(body) |> Integer.to_string},
+                            {"X-Amz-Target", "#{long_api}V#{@api_version}.#{unquote(method)}"},
+                            {"X-Amz-Date", Amazon.iso_date},
+                            {"Content-Type", "application/x-amz-json-1.1"}
+                        ]
+                    )
+
+                    rbody |> Poison.Parser.parse!
+                end
+            end
+        end
+    end
+end
--- lib/sonar/ecs.ex
+++ lib/sonar/ecs.ex
@@ -3,35 +3,15 @@ defmodule Sonar.ECS do
     Stubs for Amazon EC2 Container Service API.
 
     """
-    @api_service        "ecs"
-    @api_version        "2014-11-13"
-
     @default_aws_region "us-east-1"
 
-    alias Sonar.Core
-    alias Sonar.Utils
-    alias Sonar.Utils.Amazon
+    @api_service        {"ecs", "AmazonEC2ContainerService"}
+    @api_version        "20141113"
+    @api_methods [
+        "ListClusters",
+        "ListContainerInstances",
+        "ListServices"
+    ]
 
-    def list_clusters(auth, params) do
-        body = params
-            |> Utils.API.generate_parameter_map
-            |> Poison.encode!
-
-        Core.Auth.make_request(
-            auth[:aws_access_key],
-            auth[:aws_secret_key],
-            auth[:aws_region] || @default_aws_region,
-            @api_service,
-            "POST", "https://#{@api_service}.#{@default_aws_region}.amazonaws.com/",
-            body,
-            [
-                {"Host", "#{@api_service}.#{@default_aws_region}.amazonaws.com"},
-                {"Accept-Encoding", "identity"},
-                {"Content-Length", String.length(body) |> Integer.to_string},
-                {"X-Amz-Target", "AmazonEC2ContainerServiceV20141113.ListClusters"},
-                {"X-Amz-Date", Amazon.iso_date},
-                {"Content-Type", "application/x-amz-json-1.1"}
-            ]
-        )
-    end
+    use Sonar.Core.API
 end
